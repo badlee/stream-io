77ec417ec273c02cb951151a091c1e2c4dabe046
Badinga Ulrich <badinga.ulrich@gmail.com>
2021-04-12 18:03:26 +0100

    add admin interface

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,19 +1,55 @@
 package main
 
 import (
-	"io"
+	"embed"
+	"fmt"
+	"io/fs"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
 )
 
+// Embed a directory
+//go:embed public/*
+var embedDirStatic embed.FS
+
 func main() {
-	// Hello world, the web server
+	debugValue, hasDev := os.LookupEnv("DEBUG")
+	isProd := !hasDev || debugValue == ""
+	app := fiber.New(fiber.Config{
+		Prefork:               isProd,
+		ReduceMemoryUsage:     isProd,
+		DisableStartupMessage: isProd,
+		ServerHeader:          "Stream::IO",
+		ErrorHandler: func(ctx *fiber.Ctx, err error) error {
+			// Retreive the custom statuscode if it's an fiber.*Error
+			if e, ok := err.(*fiber.Error); ok {
+				code := e.Code
+				log.Print(fmt.Sprintln("Code %d", code))
+				if code != 404 {
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
-	}
+					// Send custom error page
+					err = ctx.Status(code).SendFile(fmt.Sprintf("./%d.html", code))
+					if err != nil {
+						// In case the SendFile fails
+						return ctx.Status(500).SendString("Internal Server Error")
+					}
+				}
+			}
+			log.Println("Redirect")
+			// Return from handler
+			ctx.Redirect("/")
+			return nil
+		},
+	})
 
-	http.HandleFunc("/hello", helloHandler)
-    log.Println("Listing for requests at http://localhost:8000/hello")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	subFS, _ := fs.Sub(embedDirStatic, "public")
+	app.Use("/", filesystem.New(filesystem.Config{
+		Root: http.FS(subFS),
+	}))
+
+	log.Fatal(app.Listen(":3000"))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,22 @@
 module github.com/badlee/stream-io
 
-go 1.15
+go 1.16
+
+require (
+	github.com/armon/go-metrics v0.3.6 // indirect
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/gobuffalo/here v0.6.2 // indirect
+	github.com/gofiber/fiber/v2 v2.7.1 // indirect
+	github.com/hashicorp/go-hclog v0.16.0 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.0 // indirect
+	github.com/hashicorp/go-msgpack v1.1.5 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/hashicorp/raft v1.2.0 // indirect
+	github.com/hashicorp/raft-boltdb v0.0.0-20210409134258-03c10cc3d4ea // indirect
+	github.com/klauspost/compress v1.11.13 // indirect
+	github.com/markbates/pkger v0.17.1 // indirect
+	github.com/valyala/fasthttp v1.23.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	go.etcd.io/bbolt v1.3.5 // indirect
+	golang.org/x/sys v0.0.0-20210403161142-5e06dd20ab57 // indirect
+)
